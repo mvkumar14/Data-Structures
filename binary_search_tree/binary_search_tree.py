16a5e15a924aceb5dc8c30f4089419a032eee35e
Kumar Veeravel <mvkumar14@gmail.com>
2020-04-23 12:47:30 -0700

    binary search tree completed (no stretch)

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -28,8 +28,22 @@ class BinarySearchTree:
                     compare_node.left = BinarySearchTree(value)
                     break
             elif value == compare_node.value:
+                # I have to "squeeze in" the node
+                # on the right. 
+                # So I have to point to the node. and then 
+                # make the new node point to 
+                # the old value.
                 
-        pass
+                new_node = BinarySearchTree(value)
+                compare_node.right, new_node.right = new_node, compare_node.right
+
+                # alternate method to acheive above result in easier to read
+                # format
+                # point_to = compare_node.right
+                # compare_node.right = BinarySearchTree(value)
+                # compare_node.right.right = point_to
+                break
+
 
     # Return True if the tree contains the value
     # False if it does not
@@ -62,8 +76,10 @@ class BinarySearchTree:
     def for_each(self, cb):
         cb(self.value)
         if self.right:
+            # print('r')
             self.right.for_each(cb)
         if self.left:
+            # print('l')
             self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
@@ -91,19 +107,70 @@ class BinarySearchTree:
     
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
-    def bft_print(self, node):
+    def bft_print(self, node=None):
+        q = Queue()
         if node == None:
-        print(self.value)
-        print()
+            current_node = self
+        else:
+            current_node = node
+        q.enqueue(current_node)
+        # remember you can't call it 
+        while q.len() > 0:
+            current_node = q.dequeue()
+            print(current_node.value)
+            if current_node.left: # exists (or "is not None")
+                q.enqueue(current_node.left)
+            if current_node.right:
+                q.enqueue(current_node.right)
         # print self, left right, then print
         # lr of the left then lr of the right
         # and so forth 
         # we are printing horizontally at every level.
+        
+        # the following is depth first ...
+        # it doesn't even use the q
+        # put self on queue
+        # call on left of self
+            # put left on queue 
+            # call on left left
+                # put ll on q
+                # call on lll
+                # call on llr
+            # call on left right
+        # call on right of self 
+
+        # remove value from queue and do the same
+        # remove value from queue and recursivley call value(if not none)
+
+        # use a queue
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
+    def dft_print(self, node=None):
+        my_stack = Stack()
+        if node == None:
+            current_node = self
+        else:
+            current_node = node
+        my_stack.push(current_node)
+        while my_stack.len() > 0:
+            current_node = my_stack.pop()
+            print(current_node.value)
+            if current_node.right: # exists:
+                my_stack.push(current_node.right)
+            if current_node.left:
+                my_stack.push(current_node.left)
+
+        # put self on stack
+        # current = pop from stack 
+        # print current
+        # put current.right on stack
+        # put current.left on stack.
+        # repeat
+        # 
+
+        # use a stack
         pass
 
     # STRETCH Goals -------------------------
@@ -116,3 +183,15 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
+
+
+bst = BinarySearchTree(1)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
+
+bst.dft_print()
